b7a1fc2b9227fc19c4be2d8272904e9f76f72682
KennethJT1 <112853926+KennethJT1@users.noreply.github.com>
2022-09-12 20:57:42 +0100

    Update index.js

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+ function transpose(array) {
+  let arr=[];
 
+for(let i=0;i<array.length;i++){
+    arr.push([])
+
+    for(let j=0;j<array.length;j++){
+        arr[i].push(array[j][i])
+    }
+}
+return arr
+}
+console.log(transpose)
+ 
 module.exports = transpose;
